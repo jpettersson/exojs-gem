8192d7104af71567e3ac4356036ae49e1590ebed
Jonathan Pettersson <jonathan.anders.pettersson@gmail.com>
2013-03-26 23:59:03 -0400

    Added a Rails engine to get rails to load the js assets

--- lib/exojs.rb
+++ lib/exojs.rb
@@ -1,5 +1,13 @@
 module ExoJS
 
+  if defined?(Rails)
+    module Rails
+      class Engine < ::Rails::Engine
+        # Get rails to add app, lib, vendor to load path
+      end
+    end
+  end
+
 end
 
 require_relative 'exojs/version'
